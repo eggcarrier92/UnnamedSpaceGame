f2044a68379557150880e31911b2f1f112c545a4
eggcarrier92 <megaalex112@gmail.com>
2022-07-23 12:09:22 +0200

    Correction

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,7 +6,8 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private int _amountOfPoints = 150;
     [SerializeField] private float _distanceBetweenPoints = 3f;
     [SerializeField] private float _maxHeight = 5f;
-    [SerializeField] private float _perlinNoiseScale = .5f;
+    [SerializeField] private float _tangentScale = 1f;
+    [SerializeField, Range(0f, 1f)] private float _perlinNoiseScale = .5f;
     [SerializeField] private float _perlinNoiseOffset = 0f;
 
     private SpriteShapeController _shape;
@@ -32,7 +33,6 @@ public class TerrainGenerator : MonoBehaviour
         {
             float xPos = _shape.spline.GetPosition(i + 1).x + _distanceBetweenPoints;
             _shape.spline.InsertPointAt(i + 2, new Vector3(xPos, _maxHeight * Mathf.PerlinNoise(i * _perlinNoiseScale + _perlinNoiseOffset, 0)));
-
         }
 
         // Set tangents
@@ -41,8 +41,7 @@ public class TerrainGenerator : MonoBehaviour
             Vector3 directionToPreviousPoint = (_shape.spline.GetPosition(i + 1) - _shape.spline.GetPosition(i + 2)).normalized;
             Vector3 directionToNextPoint = (_shape.spline.GetPosition(i + 3) - _shape.spline.GetPosition(i + 2)).normalized;
             Vector3 average = (directionToPreviousPoint - directionToNextPoint) / 2;
-            average = average.normalized;
-            Debug.Log(average);
+            average = average.normalized * _tangentScale;
 
             _shape.spline.SetTangentMode(i + 2, ShapeTangentMode.Continuous);
             _shape.spline.SetLeftTangent(i + 2, average);
@@ -56,20 +55,20 @@ public class TerrainGenerator : MonoBehaviour
                 _shape.spline.RemovePointAt(i);
 
         _shape.spline.SetPosition(0, new Vector3(-1, -1));
-        _shape.spline.SetLeftTangent (0, new Vector3(1, 0));
-        _shape.spline.SetRightTangent(0, new Vector3(-1, 0));
+        _shape.spline.SetLeftTangent (0, new Vector3(1, 0) * _tangentScale);
+        _shape.spline.SetRightTangent(0, new Vector3(-1, 0) * _tangentScale);
 
         _shape.spline.SetPosition(1, new Vector3(-1,  1));
-        _shape.spline.SetLeftTangent (1, new Vector3(-1, 0));
-        _shape.spline.SetRightTangent(1, new Vector3(1, 0));
+        _shape.spline.SetLeftTangent (1, new Vector3(-1, 0) * _tangentScale);
+        _shape.spline.SetRightTangent(1, new Vector3(1, 0) * _tangentScale);
 
         _shape.spline.SetPosition(2, new Vector3(1,  1));
-        _shape.spline.SetLeftTangent (2, new Vector3(-1, 0));
-        _shape.spline.SetRightTangent(2, new Vector3(1, 0));
+        _shape.spline.SetLeftTangent (2, new Vector3(-1, 0) * _tangentScale);
+        _shape.spline.SetRightTangent(2, new Vector3(1, 0) * _tangentScale);
 
         _shape.spline.SetPosition(3, new Vector3(1, -1));
-        _shape.spline.SetLeftTangent (3, new Vector3(1, 0));
-        _shape.spline.SetRightTangent(3, new Vector3(-1, 0));
+        _shape.spline.SetLeftTangent (3, new Vector3(1, 0) * _tangentScale);
+        _shape.spline.SetRightTangent(3, new Vector3(-1, 0) * _tangentScale);
 
         _shape.spline.isOpenEnded = false;
     }
