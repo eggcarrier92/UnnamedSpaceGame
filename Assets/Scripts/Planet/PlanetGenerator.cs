23c4d85804a80e3a62e7cebc7f34a5e84898e611
Alex Kotov <megaalex112@gmail.com>
2022-10-09 15:51:24 +0200

    Prepared for adding chunks

--- Assets/Scripts/Planet/PlanetGenerator.cs
+++ Assets/Scripts/Planet/PlanetGenerator.cs
@@ -1,95 +1,127 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    [SerializeField] private float _planetRadius = 100f;
+    [Header("Generation")]
 
-    [SerializeField, Tooltip("The angular distance between the points of the Sprite Shape. This value will be rounded to the closest value that 360 is entirely divisible by.")] 
-    private float _angularDistanceBetweenPoints = 1f;
-
+    [SerializeField] 
+    private float _planetRadius = 100f;
+    [SerializeField, Tooltip("The distance between the points of the Sprite Shape. This value will be rounded to the closest value that the planet's circumference is entirely divisible by.")] 
+    private float _distanceBetweenPoints = 20f;
     [SerializeField, Tooltip("Scale of each tangent of each point of the Sprite Shape.")] 
     private float _tangentScale = 1f;
+    [SerializeField] 
+    private float _maxHillHeight = 10f;
+    [SerializeField, Range(0f, 1f)] 
+    private float _perlinNoiseScale = .5f;
+    [SerializeField] 
+    private float _perlinNoiseOffset = 0f;
 
-    [SerializeField, Tooltip("Size of one chunk in meters. This value will be rounded to the closest value that 360 is entirely divisible by.")]
-    private float _chunkSize = 100f;
+    /*-------------------------------------------------------------*/
+    [Header("Chunk options")]
 
-    [SerializeField] private float _maxHillHeight = 10f;
-    [SerializeField, Range(0f, 1f)] private float _perlinNoiseScale = .5f;
-    [SerializeField] private float _perlinNoiseOffset = 0f;
-    [SerializeField] private bool _isOpenEnded = false;
+    [SerializeField, Tooltip("Size of one chunk in meters. This value will be rounded to the closest value that the planet's circumference is entirely divisible by.")]
+    private float _chunkSize = 1000f;
+    [SerializeField] 
+    private bool _chunkHasAPointInTheCenter = true;
+    [SerializeField] 
+    private bool _isOpenEnded = false;
 
-    private SpriteShapeController _shape;
+    /*-------------------------------------------------------------*/
 
+    private List<SpriteShapeController> _chunks;
 
     public void Generate()
     {
         InitializeComponents();
         ResetTerrain();
 
-        // Change the values so that 360 degrees is entirely divisible by the angular distance between points
-        int amountOfPoints = Mathf.RoundToInt(360f / _angularDistanceBetweenPoints);
-        float angularDistanceBetweenPoints = 360f / amountOfPoints;
-        amountOfPoints = Mathf.RoundToInt(360f / angularDistanceBetweenPoints);
-        angularDistanceBetweenPoints = 360f / amountOfPoints;
+        float planetCircumference = 2 * Mathf.PI * _planetRadius;
+        float angularDistanceBetweenPoints = 360f * _distanceBetweenPoints / planetCircumference;
+        float angularSizeOfChunk = 360f * _chunkSize / planetCircumference;
 
+        int amountOfPoints = RoundToClosestEntireDivisor(360f, ref angularDistanceBetweenPoints);
+        int amountOfChunks = RoundToClosestEntireDivisor(360f, ref angularSizeOfChunk);
+
+        //Debug.Log($"Amount of chunks: {amountOfChunks}, angular size of a chunk: {angularSizeOfChunk}");
         //Debug.Log($"Amount of points: {amountOfPoints}, angular distance between points: {angularDistanceBetweenPoints}");
+
 
         // Set points
         for (int i = 0; i < amountOfPoints; i++)
         {
-            float distanceToPlanetCenter = 
+            float distanceToPlanetCenter =
                 _planetRadius + _maxHillHeight * Mathf.PerlinNoise(i * _perlinNoiseScale + _perlinNoiseOffset, 0);
             Vector3 pointCoordinates = new(
                 distanceToPlanetCenter * Mathf.Sin(angularDistanceBetweenPoints * i * Mathf.Deg2Rad),
                 distanceToPlanetCenter * Mathf.Cos(angularDistanceBetweenPoints * i * Mathf.Deg2Rad));
-            if (i > _shape.spline.GetPointCount() - 1)
-                _shape.spline.InsertPointAt(i, pointCoordinates);
+            if (i > _chunks[0].spline.GetPointCount() - 1)
+                _chunks[0].spline.InsertPointAt(i, pointCoordinates);
             else
-                _shape.spline.SetPosition(i, pointCoordinates);
+                _chunks[0].spline.SetPosition(i, pointCoordinates);
         }
         // Set tangents
-        for (int i = 0; i < _shape.spline.GetPointCount(); i++)
+        for (int i = 0; i < _chunks[0].spline.GetPointCount(); i++)
         {
             int previousPointIndex = i - 1;
             if (i - 1 < 0)
-                previousPointIndex = _shape.spline.GetPointCount() - 1;
+                previousPointIndex = _chunks[0].spline.GetPointCount() - 1;
             int nextPointIndex = i + 1;
-            if (i + 1 > _shape.spline.GetPointCount() - 1)
+            if (i + 1 > _chunks[0].spline.GetPointCount() - 1)
                 nextPointIndex = 0;
 
-            Vector3 directionToPreviousPoint = (_shape.spline.GetPosition(previousPointIndex) - _shape.spline.GetPosition(i)).normalized;
-            Vector3 directionToNextPoint = (_shape.spline.GetPosition(nextPointIndex) - _shape.spline.GetPosition(i)).normalized;
+            Vector3 directionToPreviousPoint = (_chunks[0].spline.GetPosition(previousPointIndex) - _chunks[0].spline.GetPosition(i)).normalized;
+            Vector3 directionToNextPoint = (_chunks[0].spline.GetPosition(nextPointIndex) - _chunks[0].spline.GetPosition(i)).normalized;
             Vector3 average = (directionToPreviousPoint - directionToNextPoint) / 2;
             average = average.normalized * _tangentScale;
 
-            _shape.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-            _shape.spline.SetLeftTangent(i, average);
-            _shape.spline.SetRightTangent(i, -average);
+            _chunks[0].spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+            _chunks[0].spline.SetLeftTangent(i, average);
+            _chunks[0].spline.SetRightTangent(i, -average);
+        }
+        // Add a point in the center
+        if (_chunkHasAPointInTheCenter)
+        {
+            _chunks[0].spline.InsertPointAt(amountOfPoints, Vector2.zero);
         }
     }
+
+    private int RoundToClosestEntireDivisor(float dividend, ref float divisor)
+    {
+        int quotient = Mathf.RoundToInt(dividend / divisor);
+        divisor = dividend / quotient;
+        quotient = Mathf.RoundToInt(dividend / divisor);
+        divisor = dividend / quotient;
+        return quotient;
+    }
+
     private void InitializeComponents()
     {
-        _shape = GetComponent<SpriteShapeController>();
+        //_shape = GetComponent<SpriteShapeController>();
+        _chunks = GetComponentsInChildren<SpriteShapeController>().ToList();
     }
     private void ResetTerrain()
     {
-        _shape.spline.isOpenEnded = _isOpenEnded;
 
-        LeavePoints(2);
+        foreach(var chunk in _chunks)
+        {
+            chunk.spline.isOpenEnded = _isOpenEnded;
+            // Leave 2 points in a chunk
+            int howManyPointsToLeave = 2;
+            if (chunk.spline.GetPointCount() > 0)
+                for (int i = chunk.spline.GetPointCount() - 1; i > howManyPointsToLeave - 1; i--)
+                    chunk.spline.RemovePointAt(i);
 
-        _shape.spline.SetPosition(0, new Vector3(0, 0));
-        _shape.spline.SetLeftTangent(0, new Vector3(-1, 0) * _tangentScale);
-        _shape.spline.SetRightTangent(0, new Vector3(1, 0) * _tangentScale);
+            chunk.spline.SetPosition(0, new Vector3(0, 0));
+            chunk.spline.SetLeftTangent(0, new Vector3(-1, 0) * _tangentScale);
+            chunk.spline.SetRightTangent(0, new Vector3(1, 0) * _tangentScale);
 
-        _shape.spline.SetPosition(1, new Vector3(1, 0));
-        _shape.spline.SetLeftTangent(1, new Vector3(-1, 0) * _tangentScale);
-        _shape.spline.SetRightTangent(1, new Vector3(1, 0) * _tangentScale);
-    }
-    private void LeavePoints(int howManyPointsToLeave) 
-    {
-        if (_shape.spline.GetPointCount() > 0)
-            for (int i = _shape.spline.GetPointCount() - 1; i > howManyPointsToLeave - 1; i--)
-                _shape.spline.RemovePointAt(i);
+            chunk.spline.SetPosition(1, new Vector3(1, 0));
+            chunk.spline.SetLeftTangent(1, new Vector3(-1, 0) * _tangentScale);
+            chunk.spline.SetRightTangent(1, new Vector3(1, 0) * _tangentScale);
+        }
     }
 }
